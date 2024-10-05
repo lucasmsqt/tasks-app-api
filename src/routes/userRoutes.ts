0a2ec4e2531dd18ea018389d212d370b061e9ae0
lucasmsqt <lucacsk3@gmail.com>
2024-10-05 13:49:14 -0300

    feat: adiciona rota para pegar novo token e rota de teste do token

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,9 +1,15 @@
 import { Router } from 'express';
-import { registerUser, loginUser } from '../controllers/userController';
+import { registerUser, loginUser, refreshAccessToken } from '../controllers/userController';
+import { authenticateToken } from '../middleware/authMiddleware';
 
 const router = Router();
 
 router.post('/register', registerUser);
 router.post('/login', loginUser);
+router.post('/token', refreshAccessToken);
+
+router.get('/profile', authenticateToken, (req, res) => {
+    res.json({ message: 'Este é o perfil do usuário', userId: req.user?.id })
+});
 
 export default router;
